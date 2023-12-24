f5e2fa7ee189642845eab8434e4693274e8a5adf
Ahmed Yar <144463015+ahmedyar7@users.noreply.github.com>
2023-12-24 16:45:07 +0500

    Create RepeatingLetter.cpp

---
+++ RepeatingLetter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter the string: ";
+    string str;
+    getline(cin, str);
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = i + 1; j < str.length(); j++)
+        {
+            if (str[i] == str[j])
+            {
+                cout << str[j] << " ";
+                break;
+            }
+        }
+    }
+}
