89fa36f596c12159d0ac27db5dea66ebb5fc1f93
Ahmed Yar <144463015+ahmedyar7@users.noreply.github.com>
2023-12-24 16:20:28 +0500

    Update checkplaindriom.cpp

--- checkplaindriom.cpp
+++ checkplaindriom.cpp
@@ -1,19 +1,20 @@
-// #include <iostream>
-// #include <string>
-// #include <algorithm>
-// using namespace std;
-// int main()
-// {
-//     string str;
-//     getline(cin, str);
-//     string rstr = str;
-//     reverse(rstr.begin(), rstr.end());
-//     if (rstr == str)
-//     {
-//         cout << "Plaindriom";
-//     }
-//     else
-//     {
-//         cout << "NOT_planidrom";
-//     }
-// }
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter the string :";
+    getline(cin, str);
+    string rstr = str;
+    reverse(rstr.begin(), rstr.end());
+    if (rstr == str)
+    {
+        cout << "Plaindriom";
+    }
+    else
+    {
+        cout << "NOT_planidrom";
+    }
+}
