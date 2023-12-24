ab90c7335b3fc24589fa99d6746f6ac3c38027f3
Ahmed Yar <144463015+ahmedyar7@users.noreply.github.com>
2023-12-24 16:19:36 +0500

    Create checkplaindriom.cpp

---
+++ checkplaindriom.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+// #include <string>
+// #include <algorithm>
+// using namespace std;
+// int main()
+// {
+//     string str;
+//     getline(cin, str);
+//     string rstr = str;
+//     reverse(rstr.begin(), rstr.end());
+//     if (rstr == str)
+//     {
+//         cout << "Plaindriom";
+//     }
+//     else
+//     {
+//         cout << "NOT_planidrom";
+//     }
+// }
