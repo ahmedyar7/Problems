ec238f6087ae621a267be2035a66e05e99b16f08
Ahmed Yar <144463015+ahmedyar7@users.noreply.github.com>
2023-12-29 20:33:29 +0500

    Create UniqueElement.cpp

---
+++ UniqueElement.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:	
+	int search(int A[], int N)
+	{
+	    //code
+	    int ans = 0;
+	    for(int i=0;i<N;i++)
+	    {
+	        ans = ans ^ A[i];
+	    }
+	    return ans;
+	    
+	}
+};
